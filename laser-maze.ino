726ae9823ad4e8d45bdabb82d86bc90188847923
Devin Barham <barhamd@gmail.com>
2023-06-21 21:16:49 +0200

    add multiple sensors

--- laser-maze.ino
+++ laser-maze.ino
@@ -1,13 +1,32 @@
-int sensorPin = 2;
+int sensorPin2 = 2;
+int sensorPin3 = 3;
+int sensorPin4 = 4;
+int sensorPin5 = 5;
+int sensorPin6 = 6;
+
+int sensors[5] = {sensorPin2, sensorPin3, sensorPin4, sensorPin5, sensorPin6};
+
+int sensorCount = sizeof(sensors) / sizeof(sensors[0]);
 
 void setup() {
-  Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(sensorPin, INPUT);
+  for (int i = 0; i < sensorCount; i++) {
+    pinMode(sensors[i], INPUT);
+  }
 }
 
 void loop() {
-  int sensorState = digitalRead(sensorPin);
-  digitalWrite(LED_BUILTIN, sensorState);
+  int brokenBeam = false;
+
+  for (int i = 0; i < sensorCount; i++) {
+    int sensorState = digitalRead(sensors[i]);
+
+    if (sensorState == LOW) {
+      brokenBeam = true;
+      break;
+    }
+  }
+
+  digitalWrite(LED_BUILTIN, brokenBeam ? HIGH : LOW);
   delay(1);  // delay in between reads for stability
 }
